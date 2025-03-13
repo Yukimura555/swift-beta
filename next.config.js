3c67e61d13f2678828baa414361d8c558d75186b
Yukimura555 <yukimura@toke.com>
2025-03-13 15:20:15 +0300

    test2

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   images: {
     unoptimized: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true
+  }
 }
 
 module.exports = nextConfig 
